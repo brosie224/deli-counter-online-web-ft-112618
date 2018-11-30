2059026d5d2f83e329a8b5329a73080048ed66ad
Brian Rosengarten <barosengarten@gmail.com>
2018-11-30 00:55:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,22 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+def take_a_number(line)
+  if line.length == 0
+    line << 1
+  else
+    line << line[-1] + 1
+  end
+  puts line.inspect
+end
+
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
   end
-
+  puts katz_deli.inspect
 end
 
 def line(katz_deli)
@@ -27,3 +36,15 @@ def line(katz_deli)
 
 
 end
+line = []
+take_a_number(line) #=> 1
+take_a_number(line) #=> 2
+
+now_serving(line) #=> 1
+
+take_a_number(line) #=> 3
+
+now_serving(line) #=> 2
+now_serving(line) #=> 3
+
+take_a_number(line) #=> 1
